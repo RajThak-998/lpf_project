57a0156d3d7bf1c7f385c7445e34d409f08b9cf4
RajThak-998 <rajdsingh998@gmail.com>
2024-12-19 04:08:14 +0530

    Define Participant model and register in admin

--- participants/models.py
+++ participants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Participant(models.Model):
+    uid = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
